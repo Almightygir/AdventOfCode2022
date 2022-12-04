dc4adb3c6d202b04a4bdacc7591229efce897487
Almightygir <lee_devonald@crazyferretstudios.com>
2022-12-04 14:14:40 +0000

    Day4 Part1

--- Advent22/Advent.cs
+++ Advent22/Advent.cs
@@ -10,9 +10,10 @@ namespace Advent22
         {
             Console.WriteLine("Advent Of Code 2022\n");
 
-            //DayOne.Day_One();
-            //DayTwo.Day_Two();
+            DayOne.Day_One();
+            DayTwo.Day_Two();
             DayThree.Day_Three();
+            DayFour.Day_Four();
 
             Console.ReadLine();
         }
---
+++ Advent22/DayFour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent22
+{
+    internal class DayFour
+    {
+        public static void Day_Four()
+        {
+            Console.WriteLine("Day Four:");
+
+            string filePath = Path.Combine(Environment.CurrentDirectory, @"Data\", @"Input4.txt");
+            int redundantAssignments = 0;
+            foreach (string line in File.ReadLines(filePath))
+            {
+                if (line.Length > 0)
+                {
+                    string[] lineSplit = line.Split(',');
+                    int[,] elfRanges = new int[,] { { int.Parse(lineSplit[0].Split('-')[0]), int.Parse(lineSplit[0].Split('-')[1]) }, { int.Parse(lineSplit[1].Split('-')[0]), int.Parse(lineSplit[1].Split('-')[1]) } };
+
+                    if((elfRanges[0, 0] >= elfRanges[1, 0] && elfRanges[0, 1] <= elfRanges[1, 1]) ||
+                       (elfRanges[1, 0] >= elfRanges[0, 0] && elfRanges[1, 1] <= elfRanges[0, 1]))
+                    {
+                        redundantAssignments++;
+                    }
+                }
+            }
+            Console.WriteLine("Redundant Assignments: {0}", redundantAssignments);
+        }
+    }
+}
